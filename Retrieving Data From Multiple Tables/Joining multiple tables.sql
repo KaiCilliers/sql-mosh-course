c078b502eff2d1c4833e3cf3d5f39c6c10fed069
Kai Laptop <kaijohan428@gmail.com>
2019-08-07 22:59:51 +0200

    joining multiple tables with solution

--- Retrieving Data From Multiple Tables/Joining multiple tables.sql
+++ Retrieving Data From Multiple Tables/Joining multiple tables.sql
@@ -15,3 +15,19 @@
 -- Exercise
 -- join paymnet_methods, clients, and payments tables
 -- show relevant fields
+
+USE sql_invoicing;
+
+SELECT
+    p.date,
+    p.invoice_id,
+    p.amount,
+    c.name as client_name,
+    pm.name as 'payment method'
+FROM payments p
+JOIN clients c
+	ON p.client_id = c.client_id
+JOIN payment_methods pm
+	ON p.payment_method = pm.payment_method_id
+
+
