25f5d5e7d577844354625b31051596a057808dd8
Kai Laptop <kaijohan428@gmail.com>
2019-08-07 19:19:10 +0200

    and, or, not operatorts with exercise

---
+++ Retrieving Data From a Single Table/AND, OR, NOT Operators.sql
@@ -0,0 +1,18 @@
+SELECT customer_id, first_name, birth_date, state, points
+FROM Customers
+-- WHERE birth_date >= '1990-01-01' AND points > 1000
+-- WHERE birth_date >= '1990-01-01' OR points > 1000
+-- WHERE birth_date >= '1990-01-01'
+-- 	OR points > 1000
+-- 	AND state = 'va'
+-- WHERE birth_date > '1990-01-01' OR
+-- 	(points > 1000 AND state = 'va')
+-- WHERE NOT (birth_date > '1990-01-01'
+-- 	OR points > 1000)
+-- WHERE birth_date <= '1990-01-01'
+-- 	AND points <= 1000
+
+-- Exercise:
+-- From the order_items table, get the items
+-- 	for order #6
+--     where the total price is greater than 30
