edf9f207a918eb9aef1f3780df8e0a49e0ae4fce
Kai Laptop <kaijohan428@gmail.com>
2019-08-07 19:00:22 +0200

    where clause with exercise

---
+++ Retrieving Data From a Single Table/The WHERE Clause.sql
@@ -0,0 +1,19 @@
+-- SELECT *
+-- FROM customers
+-- WHERE points > 3000 
+
+-- Equality operators
+-- >
+-- >=
+-- <
+-- <=
+-- =
+-- != or <>
+
+-- SELECT *
+-- FROM Customers
+-- -- WHERE state = 'VA'
+-- -- WHERE state <> 'va'
+-- WHERE birth_date > '1990-01-01'
+
+-- Get the orders placed this year
