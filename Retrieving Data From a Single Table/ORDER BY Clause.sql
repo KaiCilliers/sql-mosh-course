914d81e70b1724bf8d3afdb10bf2aa780cc47c17
Kai Laptop <kaijohan428@gmail.com>
2019-08-07 22:03:49 +0200

    order by clause with solution

--- Retrieving Data From a Single Table/ORDER BY Clause.sql
+++ Retrieving Data From a Single Table/ORDER BY Clause.sql
@@ -9,3 +9,8 @@
 -- Exercise
 -- order id must be 2
 -- sort by total price in descending order
+
+SELECT *, quantity * unit_price AS total_price
+FROM order_items
+WHERE order_id = 2
+ORDER BY total_price DESC
