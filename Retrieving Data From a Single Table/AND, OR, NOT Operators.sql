8a2462b319befdc0198352eb2f7a51c86c868a6c
Kai Laptop <kaijohan428@gmail.com>
2019-08-07 19:23:42 +0200

    logical operators with solution

--- Retrieving Data From a Single Table/AND, OR, NOT Operators.sql
+++ Retrieving Data From a Single Table/AND, OR, NOT Operators.sql
@@ -1,5 +1,5 @@
-SELECT customer_id, first_name, birth_date, state, points
-FROM Customers
+-- SELECT customer_id, first_name, birth_date, state, points
+-- FROM Customers
 -- WHERE birth_date >= '1990-01-01' AND points > 1000
 -- WHERE birth_date >= '1990-01-01' OR points > 1000
 -- WHERE birth_date >= '1990-01-01'
@@ -16,3 +16,8 @@ FROM Customers
 -- From the order_items table, get the items
 -- 	for order #6
 --     where the total price is greater than 30
+
+SELECT *, unit_price * quantity AS 'total price'
+FROM order_items
+WHERE order_id = 6
+	AND unit_price * quantity > 30
