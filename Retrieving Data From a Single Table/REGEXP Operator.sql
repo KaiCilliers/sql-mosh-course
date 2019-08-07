5872f0d6084ad464392538b628336fa76fc3188b
Kai Laptop <kaijohan428@gmail.com>
2019-08-07 21:39:11 +0200

    regexp with solution

--- Retrieving Data From a Single Table/REGEXP Operator.sql
+++ Retrieving Data From a Single Table/REGEXP Operator.sql
@@ -1,12 +1,12 @@
-SELECT *
-FROM customers
+-- SELECT *
+-- FROM customers
 -- WHERE last_name REGEXP 'FIELD$' -- lastname must end with 'field'
 -- WHERE last_name REGEXP 'FIELD|MAC' -- lastname containing 'field' or mac
 -- WHERE last_name REGEXP 'field|mac|rose'
 -- WHERE last_name REGEXP '^field|mac|rose'
 -- WHERE last_name REGEXP 'field$|mac|rose'
 -- WHERE last_name REGEXP '[gim]e' -- lastname containing 'ge' or 'ie' or 'me'
-WHERE last_name REGEXP '[a-h]e'
+-- WHERE last_name REGEXP '[a-h]e'
 -- ^ beginning
 -- $ end
 -- | logical or
@@ -19,3 +19,10 @@ WHERE last_name REGEXP '[a-h]e'
     -- last names end with EY or ON
     -- last names start with MY or contains SE
     -- last names contain B followed by R or U
+
+SELECT customer_id, first_name, last_name
+FROM customers
+-- WHERE first_name REGEXP 'elka|ambur'
+-- WHERE last_name REGEXP 'ey$|on$'
+-- WHERE last_name REGEXP '^my|se'
+-- WHERE last_name REGEXP 'b[ru]'
