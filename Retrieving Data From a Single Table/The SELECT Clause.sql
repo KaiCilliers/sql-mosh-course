58daa03283fdcf2022240eb8bd8fc1967b17744c
Kai Laptop <kaijohan428@gmail.com>
2019-08-07 18:39:15 +0200

    select clause with exercise

---
+++ Retrieving Data From a Single Table/The SELECT Clause.sql
@@ -0,0 +1,18 @@
+-- SELECT *
+-- FROM customers;
+
+-- SELECT	
+-- 	last_name,
+--     first_name,
+--     points,
+--     points + 10,
+--     (points + 10) * 100 AS 'discount factor'
+-- FROM customers;
+
+-- SELECT DISTINCT state
+-- FROM customers
+
+-- Return all the products
+-- 	name
+--     unit price
+--     new price (unit price * 1.1)
