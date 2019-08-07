ae08d157a2cbaef5f2a0e8503d232d7b0c9ea3c0
Kai Laptop <kaijohan428@gmail.com>
2019-08-07 20:14:33 +0200

    in operator with exercise

---
+++ Retrieving Data From a Single Table/IN Operator.sql
@@ -0,0 +1,13 @@
+-- SELECT *
+-- FROM customers
+-- WHERE state = 'VA'
+-- 	OR state = 'GA'
+--  OR state = 'FL'
+
+-- SELECT *
+-- FROM customers
+-- WHERE state IN ('va', 'fl', 'ga')
+-- WHERE state NOT IN ('VA', 'FL', 'GA')
+
+-- Exercise
+-- Return products with quantity in stock equal to 49, 38, and 72
