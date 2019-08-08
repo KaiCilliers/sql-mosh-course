19e2a7f3ad71a6544f019bdd0ec2f82a7db4a994
Kai Laptop <kaijohan428@gmail.com>
2019-08-08 14:19:14 +0200

    updating mutiple rows with solution

--- Inserting, Updating, and Deleting Data/Updating multiple rows.sql
+++ Inserting, Updating, and Deleting Data/Updating multiple rows.sql
@@ -2,7 +2,7 @@
 -- You wont have this issue anywhere else
 -- Solution is to goto Edit -> Preferences -> MySQL Editor -> Untick safe checkbox at the bottom
 -- Then restart local DB instance
-USE sql_invoicing;
+-- USE sql_invoicing;
 
 -- UPDATE invoices
 -- SET payment_total = invoice_total * 0.5,
@@ -18,3 +18,9 @@ USE sql_invoicing;
 -- Write a SQL statement to
 -- 	give any customer born before 1990
 --     50 extra points
+
+USE sql_store;
+
+UPDATE customers
+SET points = points + 50
+WHERE birth_date < '1990-01-01'
