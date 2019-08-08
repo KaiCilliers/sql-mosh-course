866d26f8cc983ca391c8e5a53baaac38dcbc00d2
Kai Laptop <kaijohan428@gmail.com>
2019-08-08 03:17:02 +0200

    outer joins v2 with exercise

--- Retrieving Data From Multiple Tables/Outer Joins.sql
+++ Retrieving Data From Multiple Tables/Outer Joins.sql
@@ -6,7 +6,7 @@
 -- FROM customers c
 -- LEFT JOIN orders o
 -- 	ON c.customer_id = o.customer_id
--- order by c.customer_id
+-- ORDER BY c.customer_id
 
 -- SELECT
 -- 	c.customer_id,
@@ -15,17 +15,35 @@
 -- FROM orders o
 -- LEFT JOIN customers c
 -- 	ON c.customer_id = o.customer_id
--- order by c.customer_id
+-- ORDER BY c.customer_id
 
 -- Exercise
 -- show all products regardless of not having been ordered
 -- products table, orders item
 -- show product id, product name, quantity
 
+-- SELECT
+-- 	p.product_id,
+--     p.name,
+--     oi.quantity
+-- FROM products p
+-- LEFT JOIN order_items oi
+-- 	ON p.product_id = oi.product_id
+
+
+-- Best practice, always use left join and try not to use right join
 SELECT
-	p.product_id,
-    p.name,
-    oi.quantity
-FROM products p
-LEFT JOIN order_items oi
-	ON p.product_id = oi.product_id
+	c.customer_id,
+    c.first_name,
+    o.order_id,
+    sh.name as shipper
+FROM customers c
+LEFT JOIN orders o -- paste orders onto customers
+	ON o.customer_id = c.customer_id
+LEFT JOIN shippers sh -- paste shippers onto orders
+	ON o.shipper_id = sh.shipper_id
+ORDER BY c.customer_id
+
+-- Exercise
+-- Tables: order_items, order_statuses, 
+-- Required fields: order_date, order_id, first_name, shipper, status
