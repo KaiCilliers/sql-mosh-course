cba9526577f01ccd0988454a91184c194331f588
Kai Laptop <kaijohan428@gmail.com>
2019-08-08 13:55:47 +0200

    copy tables with solution

--- Inserting, Updating, and Deleting Data/Creating a copy of a table.sql
+++ Inserting, Updating, and Deleting Data/Creating a copy of a table.sql
@@ -12,3 +12,19 @@
 -- display a copy of invoices tbl
 -- client Id has to be replaced with client name
 -- show only records that have a payment(valid payment_date)
+
+USE sql_invoicing;
+
+CREATE TABLE invoices_archive AS
+SELECT
+	i.invoice_id,
+	i.number,
+    c.name AS client,
+    i.invoice_total,
+    i.payment_total,
+    i.invoice_date,
+    i.due_date,
+    i.payment_date
+FROM invoices i
+JOIN clients c USING (client_id)
+WHERE i.payment_date IS NOT NULL
