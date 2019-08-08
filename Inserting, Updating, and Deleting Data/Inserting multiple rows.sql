de11e0c95a99b36468d02c0f76a5500fbae509c1
Kai Laptop <kaijohan428@gmail.com>
2019-08-08 11:27:28 +0200

    inserting multiple rows with solution

--- Inserting, Updating, and Deleting Data/Inserting multiple rows.sql
+++ Inserting, Updating, and Deleting Data/Inserting multiple rows.sql
@@ -1,7 +1,12 @@
-INSERT INTO shippers (name)
-VALUES	('Shipper1'),
-		('Shipper2'),
-		('Shipper3')
-        
+-- INSERT INTO shippers (name)
+-- VALUES	('Shipper1'),
+-- 		('Shipper2'),
+-- 		('Shipper3')
+
 -- Exercise
 -- Insert three rows in the products table
+
+INSERT INTO products (name, quantity_in_stock, unit_price)
+VALUES	('name1', 88, 32.4),
+		('name2', 77, 19.2),
+		('name3', 41, 8.8)
