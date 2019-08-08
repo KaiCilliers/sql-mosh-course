7d9ebdf9af4d3f53747ef5e030615db83eab024d
Kai Laptop <kaijohan428@gmail.com>
2019-08-08 17:09:52 +0200

    group by clause with solution

--- Summarizing Data/Group By clause.sql
+++ Summarizing Data/Group By clause.sql
@@ -22,3 +22,13 @@
 -- columns: date, payment_method, total_payments
 -- tables: payment_methods, payments
 -- on day x there was Rx total payments via Credit Card
+
+SELECT
+	p.date,
+    pm.name,
+    SUM(amount) as total_payments
+FROM payments p
+JOIN payment_methods pm
+	ON p.payment_method = pm.payment_method_id
+GROUP BY p.date, p.payment_method
+ORDER BY p.date
