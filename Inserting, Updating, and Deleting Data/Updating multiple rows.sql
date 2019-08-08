c228665c39a0ade283f76a7c94909aecd0b574b7
Kai Laptop <kaijohan428@gmail.com>
2019-08-08 14:14:13 +0200

    update multiple rows with exercise

---
+++ Inserting, Updating, and Deleting Data/Updating multiple rows.sql
@@ -0,0 +1,20 @@
+-- This would be fine, but mySQL workbench runs in safe update mode
+-- You wont have this issue anywhere else
+-- Solution is to goto Edit -> Preferences -> MySQL Editor -> Untick safe checkbox at the bottom
+-- Then restart local DB instance
+USE sql_invoicing;
+
+-- UPDATE invoices
+-- SET payment_total = invoice_total * 0.5,
+-- 	payment_date = due_date
+-- WHERE client_id = 3
+
+-- UPDATE invoices
+-- SET payment_total = invoice_total * 0.5,
+-- 	payment_date = due_date
+-- WHERE client_id IN (3,4)
+
+-- Exercise
+-- Write a SQL statement to
+-- 	give any customer born before 1990
+--     50 extra points
