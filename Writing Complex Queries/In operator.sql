3874201fc6163e853722ebe719b8149ab63ebda5
Kai Laptop <kaijohan428@gmail.com>
2019-08-08 20:04:00 +0200

    in operator with solution

--- Writing Complex Queries/In operator.sql
+++ Writing Complex Queries/In operator.sql
@@ -1,12 +1,31 @@
 -- Find items that have never been ordered
-USE sql_store;
+-- USE sql_store;
 
-SELECT *
-FROM products
-WHERE product_id NOT IN (
-	SELECT DISTINCT product_id
-	FROM order_items
-) 
+-- SELECT *
+-- FROM products
+-- WHERE product_id NOT IN (
+-- 	SELECT DISTINCT product_id
+-- 	FROM order_items
+-- ) 
 
 -- EXERCISE
 -- find clients without invoices
+
+USE sql_invoicing;
+
+-- Solution 1
+SELECT *
+FROM clients
+WHERE client_id NOT IN (
+	SELECT client_id
+	FROM invoices
+	GROUP BY client_id
+)
+
+-- Solution 2
+SELECT *
+FROM clients
+WHERE client_id NOT IN (
+	SELECT DISTINCT client_id
+	FROM invoices
+)
