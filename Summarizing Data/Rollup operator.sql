1fc0c265e536bf3eab66adcc193ac03d9c649c52
Kai Laptop <kaijohan428@gmail.com>
2019-08-08 19:13:56 +0200

    with rollup with solution

--- Summarizing Data/Rollup operator.sql
+++ Summarizing Data/Rollup operator.sql
@@ -1,12 +1,12 @@
 -- WITH ROLLUP is unique to MySQL
 -- summarizes data
-SELECT
-	state,
-    city,
-    SUM(invoice_total) as total_sales
-FROM invoices i
-JOIN clients c USING (client_id)
-GROUP BY state, city WITH ROLLUP
+-- SELECT
+-- 	state,
+--     city,
+--     SUM(invoice_total) as total_sales
+-- FROM invoices i
+-- JOIN clients c USING (client_id)
+-- GROUP BY state, city WITH ROLLUP
 
 -- You the total of all cities in a state
 -- and you get the total of all states
@@ -15,3 +15,11 @@ GROUP BY state, city WITH ROLLUP
 -- table: payments
 -- columns: payment_method and total
 -- show total of all totals
+
+SELECT
+	pm.name as payment_method,
+    SUM(p.amount) as total
+FROM payments p
+JOIN payment_methods pm
+	ON p.payment_method = pm.payment_method_id
+GROUP BY pm.name WITH ROLLUP
