c2e4a1c013476402d8ed7e2c4e855aefc34c9e33
Kai Laptop <kaijohan428@gmail.com>
2019-08-08 15:38:09 +0200

    subqueries in updates with exercise

---
+++ Inserting, Updating, and Deleting Data/Subqueries in updates.sql
@@ -0,0 +1,19 @@
+-- MySQL will execute the parenthases first, thus providing us with an ID
+-- UPDATE invoices
+-- SET
+-- 	payment_total = invoice_total * 0.5,
+--     payment_date = due_date
+-- WHERE client_id IN
+-- WHERE client_id =
+-- 			-- (SELECT client_id
+-- 			FROM clients
+-- 			WHERE name = 'MyWorks')
+-- 			(SELECT client_id
+-- 			FROM clients
+-- 			WHERE state IN ('CA', 'NY'))
+--             
+-- Above shows where subquery returns one value and multiple values
+
+-- Exercise
+-- update comments of orders where customers have more than 3000 points
+-- set comment to 'Gold Customer'
