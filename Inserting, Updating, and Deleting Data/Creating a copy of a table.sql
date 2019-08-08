9172900b4e0e907659264f6cf661e40ac239e967
Kai Laptop <kaijohan428@gmail.com>
2019-08-08 13:45:22 +0200

    copy tables with exercise

---
+++ Inserting, Updating, and Deleting Data/Creating a copy of a table.sql
@@ -0,0 +1,14 @@
+-- Note, you wont have a PK field that increments
+-- CREATE TABLE orders_archived AS
+-- -- This is a sub-query
+-- SELECT * FROM orders
+
+-- INSERT INTO orders_archived
+-- SELECT *
+-- FROM orders
+-- WHERE order1_date < '2019-01-01'
+
+-- Exercise
+-- display a copy of invoices tbl
+-- client Id has to be replaced with client name
+-- show only records that have a payment(valid payment_date)
