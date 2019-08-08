1d18cbc6b1711c90d72fea7fd66a8120ed3d12d3
Kai Laptop <kaijohan428@gmail.com>
2019-08-08 03:52:50 +0200

    using clause with solution

--- Retrieving Data From Multiple Tables/Using Clause.sql
+++ Retrieving Data From Multiple Tables/Using Clause.sql
@@ -24,3 +24,14 @@
 -- Exercise
 -- Columns: date, client_name, amount, payment_methods
 -- Tables: payment methods, clients, payments
+USE sql_invoicing;
+
+SELECT
+	p.date,
+    c.name AS client,
+    p.amount,
+    pm.name AS 'payment method'
+FROM payments p
+JOIN clients c USING (client_id)
+JOIN payment_methods pm
+	ON p.payment_method = pm.payment_method_id
