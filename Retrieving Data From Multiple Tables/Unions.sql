7f9d82971f28f28127657662a9190c152841dfb1
Kai Laptop <kaijohan428@gmail.com>
2019-08-08 04:16:32 +0200

    unions with exercise

---
+++ Retrieving Data From Multiple Tables/Unions.sql
@@ -0,0 +1,30 @@
+-- Combine results from multiple Queries
+-- Results will be added as new rows
+-- The column amount has to be equal of each table
+-- Column name is from tbl_1's column
+-- SELECT
+-- 	order_id,
+--     order_date,
+--     'Active' AS status
+-- FROM orders
+-- WHERE order_date >= '2019-01-01'
+-- UNION
+-- SELECT
+-- 	order_id,
+--     order_date,
+--     'Archived' AS status
+-- FROM orders
+-- WHERE order_date < '2019-01-01'
+
+-- SELECT first_name as full_name
+-- FROM customers -- better example table is archived_orders
+-- UNION
+-- SELECT name
+-- FROM shippers -- better example table is orders
+
+-- Exercise
+-- columns: customer_id, first_name, points, type
+-- type: <2000 points = bronze
+-- 	2000 - 3000 points = silver
+--     >3000 = gold
+-- sorted by first_name
