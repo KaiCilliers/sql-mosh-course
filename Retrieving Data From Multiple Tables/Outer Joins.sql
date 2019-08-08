67eec50864fa22753f1ed573ef70fc5548b873f1
Kai Laptop <kaijohan428@gmail.com>
2019-08-08 03:26:40 +0200

    outer joins v2 with solution

--- Retrieving Data From Multiple Tables/Outer Joins.sql
+++ Retrieving Data From Multiple Tables/Outer Joins.sql
@@ -32,18 +32,31 @@
 
 
 -- Best practice, always use left join and try not to use right join
-SELECT
-	c.customer_id,
-    c.first_name,
-    o.order_id,
-    sh.name as shipper
-FROM customers c
-LEFT JOIN orders o -- paste orders onto customers
-	ON o.customer_id = c.customer_id
-LEFT JOIN shippers sh -- paste shippers onto orders
-	ON o.shipper_id = sh.shipper_id
-ORDER BY c.customer_id
+-- SELECT
+-- 	c.customer_id,
+--     c.first_name,
+--     o.order_id,
+--     sh.name as shipper
+-- FROM customers c
+-- LEFT JOIN orders o -- paste orders onto customers
+-- 	ON o.customer_id = c.customer_id
+-- LEFT JOIN shippers sh -- paste shippers onto orders
+-- 	ON o.shipper_id = sh.shipper_id
+-- ORDER BY c.customer_id
 
 -- Exercise
 -- Tables: order_items, order_statuses, 
 -- Required fields: order_date, order_id, first_name, shipper, status
+SELECT
+	o.order_date,
+    o.order_id,
+    c.first_name,
+    sh.name AS shipper,
+    os.name as status
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id
+LEFT JOIN shippers sh
+	ON o.shipper_id = sh.shipper_id
+JOIN order_statuses os
+	ON o.status = os.order_status_id
