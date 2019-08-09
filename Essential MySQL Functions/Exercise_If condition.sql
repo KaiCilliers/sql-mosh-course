3b90f1021a26b046f437155a757c1e5b69a9b8b6
Kai Laptop <kaijohan428@gmail.com>
2019-08-09 14:59:20 +0200

    if condition with solution

--- Essential MySQL Functions/Exercise_If condition.sql
+++ Essential MySQL Functions/Exercise_If condition.sql
@@ -1,2 +1,30 @@
 -- columns: product id, prod name, amount of orders for item,
 -- frequency (orders = 1 then 'Once' else 'Many times')
+
+-- my solution
+SELECT
+	oi.product_id,
+    p.name,
+    COUNT(oi.product_id) as orders,
+    IF(
+		COUNT(oi.product_id) > 1,
+        'Many times',
+        'Once'
+    ) as frequency
+FROM order_items oi
+JOIN products p USING (product_id)
+GROUP BY oi.product_id;
+
+-- different approach (arguably better)
+SELECT
+	product_id,
+    name,
+    COUNT(*) as orders,
+    IF(
+		COUNT(*) > 1,
+        'Many times',
+        'Once'
+    ) as fequency
+FROM products
+JOIN order_items USING (product_id)
+GROUP BY product_id, name
