5b41d4e81b45d72d24cd0dc92216ab15861297a4
Kai Laptop <kaijohan428@gmail.com>
2019-08-09 19:54:09 +0200

    create stoerd procedure with solution

--- Stored Procedures/Creating a stored procedure.sql
+++ Stored Procedures/Creating a stored procedure.sql
@@ -15,3 +15,12 @@ CALL get_clients();
 -- create a stored procedure called
 -- 		get_invoices_with_balance
 -- 		to return all the invoices with a balance > 0
+
+DELIMITER $$
+	CREATE PROCEDURE get_invoices_with_balance()
+    BEGIN
+		SELECT *
+        FROM invoices_with_balance
+        WHERE balance > 0;
+    END$$
+DELIMITER ;
