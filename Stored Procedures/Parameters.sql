7e7246ffc271759f3b66a5d9d2083e73a4c058cd
Kai Laptop <kaijohan428@gmail.com>
2019-08-09 20:37:22 +0200

    parameters with solution

--- Stored Procedures/Parameters.sql
+++ Stored Procedures/Parameters.sql
@@ -15,3 +15,15 @@ CALL get_clients_by_state('CA');
 -- Exercise
 -- write a stored procedure to return invoices for a given client
 -- get_invoices_by_client
+DROP PROCEDURE IF EXISTS get_invoices_by_client;
+DELIMITER $$
+	CREATE PROCEDURE get_invoices_by_client(
+		client_id INT
+    )
+    BEGIN
+		SELECT *
+		FROM invoices i
+		WHERE i.client_id = client_id;
+    END$$
+DELIMITER ;
+CALL get_invoices_by_client(5);
