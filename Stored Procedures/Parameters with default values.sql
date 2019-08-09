32b1a18d40a3f86ad45181218162730d456b2b6d
Kai Laptop <kaijohan428@gmail.com>
2019-08-09 21:10:21 +0200

    parameters with default values with exercise

---
+++ Stored Procedures/Parameters with default values.sql
@@ -0,0 +1,43 @@
+-- one way
+-- DROP PROCEDURE IF EXISTS get_clients_by_state;
+-- DELIMITER $$
+-- 	CREATE PROCEDURE get_clients_by_state(
+-- 		state CHAR(2)
+--     )
+--     BEGIN
+-- 		IF state IS NULL THEN
+--         -- What if you want to return all the records by default
+-- 			-- SET state = 'CA';
+-- 		-- Return all solution :)
+--             SELECT * FROM clients;
+-- 		ELSE
+-- 			SELECT * FROM clients c
+-- 			WHERE c.state = state;
+-- 		END IF;		
+--     END$$
+-- DELIMITER ;
+-- CALL get_clients_by_state();
+
+-- second way
+-- DROP PROCEDURE IF EXISTS get_clients_by_state;
+-- DELIMITER $$
+-- 	CREATE PROCEDURE get_clients_by_state(
+-- 		state CHAR(2)
+--     )
+--     BEGIN
+-- 		SELECT * FROM clients c
+--         -- when passing NULL
+--         -- you end up with:
+--         -- WHERE 1 = 1 which equals true, and returns all records
+-- 		WHERE c.state = IFNULL(state, c.state);
+--     END$$
+-- DELIMITER ;
+-- CALL get_clients_by_state(NULL);
+
+-- Exercise
+-- Write a stored procedure called get_payments
+-- with two parameters
+-- 	client_id +> INT - takes 4 bytes
+--     payment_method => TINYINT - takes 1 byte
+-- by default, return all payments
+-- Return results even if one id is provided
