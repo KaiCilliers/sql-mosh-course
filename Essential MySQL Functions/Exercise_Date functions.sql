b410a710b1c8bd201616e101566206dafc6914b9
Kai Laptop <kaijohan428@gmail.com>
2019-08-09 13:58:02 +0200

    date functions with solution

--- Essential MySQL Functions/Exercise_Date functions.sql
+++ Essential MySQL Functions/Exercise_Date functions.sql
@@ -1,4 +1,15 @@
 -- Rewrite using date functions
+-- SELECT *
+-- FROM orders
+-- WHERE order_date >= '2019-01-01';
+
+
+-- My solution
 SELECT *
 FROM orders
-WHERE order_date >= '2019-01-01'
+WHERE EXTRACT(YEAR FROM order_date) = '2019';
+
+-- Current year, above is for 2019 specifically
+SELECT *
+FROM orders
+WHERE YEAR(order_date) = YEAR(NOW())
