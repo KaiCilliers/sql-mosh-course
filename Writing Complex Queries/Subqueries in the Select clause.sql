14b91796a8edfddbc89fba6523e7cbc60b4fbc9f
Kai Laptop <kaijohan428@gmail.com>
2019-08-09 12:37:20 +0200

    subqueries in select with solution

--- Writing Complex Queries/Subqueries in the Select clause.sql
+++ Writing Complex Queries/Subqueries in the Select clause.sql
@@ -32,6 +32,25 @@
 -- average of all invoices, difference
 -- Show all clients
 
+-- My Solution
+-- SELECT
+-- 	c.client_id,
+--     c.name,
+--     SUM(i.invoice_total) as total_sales,
+--     (SELECT AVG(invoice_total)
+-- 		FROM invoices) as average,
+-- 	SUM(i.invoice_total) - (SELECT average) as difference
+-- FROM clients c
+-- LEFT JOIN invoices i USING (client_id)
+-- GROUP BY c.client_id
 
-
+SELECT
+	client_id,
+    name,
+    (SELECT SUM(invoice_total) -- correlated subquery
+		FROM invoices
+        WHERE client_id = c.client_id) as total_sales,
+	(SELECT AVG(invoice_total) FROM invoices) as average,
+    (SELECT total_sales - average) as difference
+FROM clients c
 
