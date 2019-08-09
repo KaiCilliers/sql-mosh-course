d1eb0331f46595e4f06565e6445814581d1ab220
Kai Laptop <kaijohan428@gmail.com>
2019-08-09 11:51:29 +0200

    exists operator with solution

--- Writing Complex Queries/Exists operator Exercise.sql
+++ Writing Complex Queries/Exists operator Exercise.sql
@@ -1,2 +1,17 @@
 -- Find the products that have never been ordered
 USE sql_store;
+
+SELECT *
+FROM products p
+WHERE product_id NOT IN( -- returns a list of ID's
+	SELECT DISTINCT product_id
+	FROM order_items
+)
+
+SELECT *
+FROM products p
+WHERE NOT EXISTS ( -- returns true and false for each record in products
+	SELECT DISTINCT product_id
+	FROM order_items
+    WHERE product_id = p.product_id
+)
