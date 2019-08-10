4c267476c604c8b68bd430a484581410460345a5
Kai Laptop <kaijohan428@gmail.com>
2019-08-10 13:49:29 +0200

    triggers with solution

--- Triggers and Events/Triggers.sql
+++ Triggers and Events/Triggers.sql
@@ -36,3 +36,18 @@ VALUES (
 -- Exercise
 -- create a trigger that gets fired when we
 -- delete a payment
+DELIMITER $$
+CREATE TRIGGER payment_after_delete
+	AFTER DELETE ON payments
+    FOR EACH ROW
+BEGIN
+	UPDATE invoices
+    SET payment_total = payment_total - OLD.amount
+    WHERE invoice_id = OLD.invoice_id;
+END$$
+DELIMITER ;
+
+-- Test
+DELETE
+FROM payments
+WHERE payment_id = 9
