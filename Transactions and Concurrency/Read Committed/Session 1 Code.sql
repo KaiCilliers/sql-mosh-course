60272a0b1f7f85d7db3fa3d866c9f7716461b695
Kai Laptop <kaijohan428@gmail.com>
2019-08-10 16:10:04 +0200

    isolation level: read committed

---
+++ Transactions and Concurrency/Read Committed/Session 1 Code.sql
@@ -0,0 +1,7 @@
+USE sql_store;
+
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+START TRANSACTION;
+SELECT points FROM customers WHERE customer_id = 1;
+SELECT points FROM customers WHERE customer_id = 1;
+COMMIT;
